956d1a247005e58bd969711d2488ce98bac45ff9
Eric Menendez <ericmenendez@gmail.com>
2015-01-09 21:35:09 -0500

    Auto prompt for admin privileges

--- ~LoadMapSAREXTools.bat
+++ ~LoadMapSAREXTools.bat
@@ -1,3 +1,27 @@
+@echo off
+
+REM Get admin privileges
+REM from https://sites.google.com/site/eneerge/scripts/batchgotadmin
+
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+
 @echo on
 
 If DEFINED ProgramFiles(x86) Set _programs=%ProgramFiles(x86)%
